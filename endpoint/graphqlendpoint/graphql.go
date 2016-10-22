03545ff8ab723ea7ddd940595c9aee172c7049c5
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2016-10-22 20:57:10 +1300

    Refactoring graphql implementation

--- endpoint/graphqlendpoint/graphql.go
+++ endpoint/graphqlendpoint/graphql.go
@@ -41,33 +41,7 @@ var rootQueryType = graphql.NewObject(
 	graphql.ObjectConfig{
 		Name: "RootQuery",
 		Fields: graphql.Fields{
-			"tenant": &graphql.Field{
-				Type:        tenantType,
-				Description: "Returns an existing tenant",
-				Args: graphql.FieldConfigArgument{
-					"id": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-				},
-				Resolve: func(resolveParams graphql.ResolveParams) (interface{}, error) {
-					executionContext := resolveParams.Context.Value("ExecutionContext").(executionContext)
-					id, _ := resolveParams.Args["id"].(string)
-
-					tenantID, err := system.ParseUUID(id)
-
-					if err != nil {
-						return nil, err
-					}
-
-					var returnedTenant domain.Tenant
-
-					if returnedTenant, err = executionContext.tenantService.ReadTenant(tenantID); err != nil {
-						return nil, err
-					}
-
-					return tenant{SecretKey: returnedTenant.SecretKey}, nil
-				},
-			},
+			"tenant": getTenant(),
 		},
 	},
 )
@@ -76,97 +50,9 @@ var rootMutationType = graphql.NewObject(
 	graphql.ObjectConfig{
 		Name: "RootMutation",
 		Fields: graphql.Fields{
-			"createTenant": &graphql.Field{
-				Type:        graphql.ID,
-				Description: "Creates new tenant",
-				Args: graphql.FieldConfigArgument{
-					"tenant": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(inputTenantType),
-					},
-				},
-				Resolve: func(resolveParams graphql.ResolveParams) (interface{}, error) {
-					inputTenantArgument, _ := resolveParams.Args["tenant"].(map[string]interface{})
-
-					tenant := resolveTenantFromInputTenantArgument(inputTenantArgument)
-
-					executionContext := resolveParams.Context.Value("ExecutionContext").(executionContext)
-
-					tenantID, err := executionContext.tenantService.CreateTenant(tenant)
-
-					if err != nil {
-						return nil, err
-					}
-
-					return tenantID.String(), nil
-				},
-			},
-
-			"updateTenant": &graphql.Field{
-				Type:        graphql.ID,
-				Description: "Updates existing tenant",
-				Args: graphql.FieldConfigArgument{
-					"id": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.ID),
-					},
-					"tenant": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(inputTenantType),
-					},
-				},
-				Resolve: func(resolveParams graphql.ResolveParams) (interface{}, error) {
-					inputTenantArgument, _ := resolveParams.Args["tenant"].(map[string]interface{})
-					id, _ := resolveParams.Args["id"].(string)
-
-					var tenantID system.UUID
-					var err error
-
-					if tenantID, err = system.ParseUUID(id); err != nil {
-						return nil, err
-					}
-
-					tenant := resolveTenantFromInputTenantArgument(inputTenantArgument)
-
-					executionContext := resolveParams.Context.Value("ExecutionContext").(executionContext)
-
-					err = executionContext.tenantService.UpdateTenant(tenantID, tenant)
-
-					if err != nil {
-						return nil, err
-					}
-
-					return tenantID.String(), nil
-				},
-			},
-
-			"deleteTenant": &graphql.Field{
-				Type:        graphql.ID,
-				Description: "Deletes existing tenant",
-				Args: graphql.FieldConfigArgument{
-					"id": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.ID),
-					},
-				},
-				Resolve: func(resolveParams graphql.ResolveParams) (interface{}, error) {
-					id, _ := resolveParams.Args["id"].(string)
-
-					var tenantID system.UUID
-					var err error
-
-					if tenantID, err = system.ParseUUID(id); err != nil {
-						return nil, err
-					}
-
-					executionContext := resolveParams.Context.Value("ExecutionContext").(executionContext)
-
-					err = executionContext.tenantService.DeleteTenant(tenantID)
-
-					if err != nil {
-						return nil, err
-					}
-
-					return tenantID.String(), nil
-
-				},
-			},
+			"createTenant": getCreateTenant(),
+			"updateTenant": getUpdateTenant(),
+			"deleteTenant": getDeleteTenant(),
 		},
 	},
 )
@@ -197,6 +83,134 @@ func ExecuteQuery(query string, tenantService contract.TenantService) (interface
 	}
 
 	return result, nil
+}
+
+func getTenant() *graphql.Field {
+	return &graphql.Field{
+		Type:        tenantType,
+		Description: "Returns an existing tenant",
+		Args: graphql.FieldConfigArgument{
+			"id": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(graphql.String),
+			},
+		},
+		Resolve: func(resolveParams graphql.ResolveParams) (interface{}, error) {
+			executionContext := resolveParams.Context.Value("ExecutionContext").(executionContext)
+			id, _ := resolveParams.Args["id"].(string)
+
+			tenantID, err := system.ParseUUID(id)
+
+			if err != nil {
+				return nil, err
+			}
+
+			var returnedTenant domain.Tenant
+
+			if returnedTenant, err = executionContext.tenantService.ReadTenant(tenantID); err != nil {
+				return nil, err
+			}
+
+			return tenant{SecretKey: returnedTenant.SecretKey}, nil
+		},
+	}
+}
+
+func getCreateTenant() *graphql.Field {
+	return &graphql.Field{
+		Type:        graphql.ID,
+		Description: "Creates new tenant",
+		Args: graphql.FieldConfigArgument{
+			"tenant": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(inputTenantType),
+			},
+		},
+		Resolve: func(resolveParams graphql.ResolveParams) (interface{}, error) {
+			inputTenantArgument, _ := resolveParams.Args["tenant"].(map[string]interface{})
+
+			tenant := resolveTenantFromInputTenantArgument(inputTenantArgument)
+
+			executionContext := resolveParams.Context.Value("ExecutionContext").(executionContext)
+
+			tenantID, err := executionContext.tenantService.CreateTenant(tenant)
+
+			if err != nil {
+				return nil, err
+			}
+
+			return tenantID.String(), nil
+		},
+	}
+}
+
+func getUpdateTenant() *graphql.Field {
+	return &graphql.Field{
+		Type:        graphql.ID,
+		Description: "Updates existing tenant",
+		Args: graphql.FieldConfigArgument{
+			"id": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(graphql.ID),
+			},
+			"tenant": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(inputTenantType),
+			},
+		},
+		Resolve: func(resolveParams graphql.ResolveParams) (interface{}, error) {
+			inputTenantArgument, _ := resolveParams.Args["tenant"].(map[string]interface{})
+			id, _ := resolveParams.Args["id"].(string)
+
+			var tenantID system.UUID
+			var err error
+
+			if tenantID, err = system.ParseUUID(id); err != nil {
+				return nil, err
+			}
+
+			tenant := resolveTenantFromInputTenantArgument(inputTenantArgument)
+
+			executionContext := resolveParams.Context.Value("ExecutionContext").(executionContext)
+
+			err = executionContext.tenantService.UpdateTenant(tenantID, tenant)
+
+			if err != nil {
+				return nil, err
+			}
+
+			return tenantID.String(), nil
+		},
+	}
+}
+
+func getDeleteTenant() *graphql.Field {
+	return &graphql.Field{
+		Type:        graphql.ID,
+		Description: "Deletes existing tenant",
+		Args: graphql.FieldConfigArgument{
+			"id": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(graphql.ID),
+			},
+		},
+		Resolve: func(resolveParams graphql.ResolveParams) (interface{}, error) {
+			id, _ := resolveParams.Args["id"].(string)
+
+			var tenantID system.UUID
+			var err error
+
+			if tenantID, err = system.ParseUUID(id); err != nil {
+				return nil, err
+			}
+
+			executionContext := resolveParams.Context.Value("ExecutionContext").(executionContext)
+
+			err = executionContext.tenantService.DeleteTenant(tenantID)
+
+			if err != nil {
+				return nil, err
+			}
+
+			return tenantID.String(), nil
+
+		},
+	}
 }
 
 func resolveTenantFromInputTenantArgument(inputTenantArgument map[string]interface{}) domain.Tenant {
