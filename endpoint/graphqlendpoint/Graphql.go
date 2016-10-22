9e06bd89e1f8ec3d11d8fbb16951b83e379fbf60
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2016-10-22 21:01:26 +1300

    Refactoring graphql implementation

---
+++ endpoint/graphqlendpoint/Graphql.go
@@ -0,0 +1,58 @@
+package graphqlendpoint
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/graphql-go/graphql"
+	"github.com/microbusinesses/TenantService/business/contract"
+	"golang.org/x/net/context"
+)
+
+var rootQueryType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "RootQuery",
+		Fields: graphql.Fields{
+			"tenant": getTenant(),
+		},
+	},
+)
+
+var rootMutationType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "RootMutation",
+		Fields: graphql.Fields{
+			"createTenant": getCreateTenant(),
+			"updateTenant": getUpdateTenant(),
+			"deleteTenant": getDeleteTenant(),
+		},
+	},
+)
+
+var tenantSchema, _ = graphql.NewSchema(graphql.SchemaConfig{Query: rootQueryType, Mutation: rootMutationType})
+
+type executionContext struct {
+	tenantService contract.TenantService
+}
+
+// ExecuteQuery executes the provided query and returns the result.
+func ExecuteQuery(query string, tenantService contract.TenantService) (interface{}, error) {
+	result := graphql.Do(
+		graphql.Params{
+			Schema:        tenantSchema,
+			RequestString: query,
+			Context:       context.WithValue(context.Background(), "ExecutionContext", executionContext{tenantService}),
+		})
+
+	if result.HasErrors() {
+		errorMessages := []string{}
+
+		for _, err := range result.Errors {
+			errorMessages = append(errorMessages, err.Error())
+		}
+
+		return nil, errors.New(strings.Join(errorMessages, "\n"))
+	}
+
+	return result, nil
+}
